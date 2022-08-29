7bc3c90a7a55c634b979dc0cbaa741846602f82a
Diego <tpddiego@gmail.com>
2022-08-29 20:06:12 -0300

    limpando a aplicação para iniciar o projeto

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import logo from '../../logo.svg';
-import ClassComponent from '../ClassComponent';
-import FuncionalComponent from '../FuncionalComponent';
-import TestComponent from '../TestComponent';
 import './App.css';
+
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <TestComponent/>
-          <FuncionalComponent name='Componente Funcional'/>
-          <ClassComponent name='Mundoo props'/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     App
     </div>
   );
 }
