ab277fdbacfcde457ddb22851ce760d64c353ce6
Diego <tpddiego@gmail.com>
2022-08-29 18:24:12 -0300

    cria o primeiro componente

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
+import TestComponent from '../TestComponent';
 import './App.css';
+
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <TestComponent/>
         </p>
         <a
           className="App-link"
---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import './TestComponent.css'
+export const TestComponent = () => <div className="TestComponent">Test Component</div>
+
